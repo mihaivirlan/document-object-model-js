667ea3b5915ffb8535db6d30828ad126a97eb4ca
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-12 23:26:06 +0300

    Selecting Elements - Exercises

--- script.js
+++ script.js
@@ -105,6 +105,17 @@
 
 
 
+    //Selecting Elements - Exercises
+// document.querySelector('a').style.backgroundColor = 'red';
+
+// document.querySelectorAll('.simple')[0].style.backgroundColor = 'red';
+
+// document.querySelectorAll('.simple')[1].style.backgroundColor = 'red';
+
+document.querySelector('.simple').firstElementChild.textContent = 'Hello';
+
+
+
 
 
 
