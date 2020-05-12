b2acf046bf83fb0f5d4f0011d6f8b039af53e86e
mihaivirlan <mihaivirlan93@gmail.com>
2020-05-12 15:13:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+    //The Window Object
+// console.log(window.innerWidth);
+// console.log(innerWidth);
+
+// console.log(window.innerWidth);
+// console.log(outerWidth);
+
+// console.log(window.innerHeight);
+// console.log(outerHeight);
+
+// console.log(window);
+
+// localStorage.setItem('key1', 1000);
+// console.log(localStorage.getItem('key1'));
+
+// sessionStorage.setItem('key1', 1000);
+// console.log(sessionStorage.getItem('key1'));
+
+// console.log(window.location);
+
+// console.log(window.document);
+
+// window.open('http://www.google.com')
+
+
+
+
+
+
